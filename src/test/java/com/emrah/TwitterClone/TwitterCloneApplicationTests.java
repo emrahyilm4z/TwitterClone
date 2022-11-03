81f5101a20d1b3ebc3ebe5da6fa4b0b551770e37
emrahyilm4z <emrahyilm4z@gmail.com>
2022-11-03 13:32:29 +0300

    Initial commit

---
+++ src/test/java/com/emrah/TwitterClone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrah.TwitterClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
